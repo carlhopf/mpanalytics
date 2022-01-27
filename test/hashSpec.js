3aa017ec702585d9fe9e98a470ad8ad7f1a4a2b1
Carl <carlhopf.com+git@gmail.com>
2022-01-27 09:11:12 +0000

    2.0.3, switch to URLSearchParams

--- test/hashSpec.js
+++ test/hashSpec.js
@@ -1,31 +1,31 @@
-import config from './config.js';
+//import config from './config.js';
 import hash from '../lib/hash.js';
 import { v4 } from 'uuid';
 import assert from 'assert';
 
 describe('hash', function () {
-	it('get uuid', function () {
-		var res = hash.get(v4());
-		assert.equal(typeof res, 'number');
-	});
+  it('get uuid', function () {
+    var res = hash.get(v4());
+    assert.equal(typeof res, 'number');
+  });
 
-	it('range uuid', function () {
-		var len = 100;
-		var i;
-		var map = {};
+  it('range uuid', function () {
+    var len = 100;
+    var i;
+    var map = {};
 
-		for (i = 0; i < len; i++) {
-			map[i] = 0;
-		}
+    for (i = 0; i < len; i++) {
+      map[i] = 0;
+    }
 
-		for (i = 0; i < 100000; i++) {
-			map[hash.range(v4(), len)]++;
-		}
+    for (i = 0; i < 100000; i++) {
+      map[hash.range(v4(), len)]++;
+    }
 
-		console.log(map);
+    console.log(map);
 
-		for (i = 0; i < len; i++) {
-			assert.notEqual(map[i], 0);
-		}
-	});
+    for (i = 0; i < len; i++) {
+      assert.notEqual(map[i], 0);
+    }
+  });
 });
